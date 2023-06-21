cbf7010fe3469b525e6287e7165fbcdd6f2bfcbd
lisancardia <lisa.cardia@shrss.com>
2023-06-21 16:28:53 -0400

    Added the search engine, you can press enter or search

--- src/app.js
+++ src/app.js
@@ -37,10 +37,19 @@ function displayTemperature(response) {
     iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttriubte("alt", response.data.weather[0].description);
 }
+function search(city) {
+    let apiKey = "7784a4cd4aa2e0c25ead7bd96d585b8a";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`; 
+    axios.get(apiUrl).then(displayTemperature);
+}
 
-let apiKey = "7784a4cd4aa2e0c25ead7bd96d585b8a";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+search("New York");
 
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);

5c87dce0ed43dcefbc5524e4ace05a6cbdf3cc98
lisancardia <lisa.cardia@shrss.com>
2023-06-27 15:36:58 -0400

    use previous api to connect to the next api
    Initial response should then help us call the one with coordinates (of longitude and latitude)
    
    response looks like an array
    
    use previous api to connect to the next api

--- src/app.js
+++ src/app.js
@@ -19,16 +19,16 @@ function formatDate(timestamp) {
     let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
 }
+function displayForecast(response) {
 
-function displayForecast() {
     let forecastElement = document.querySelector("#forecast");
 
     let days = ["Thu", "Fri", "Sat", "Sun"];
-    
+
     let forecastHTML = `<div class=row>`;
     days.forEach(function(day) {
-    forecastHTML = 
-        forecastHTML +
+        forecastHTML = 
+            forecastHTML +
             `
                 <div class="col-2">
                     <div class="weather-forecast-date">${day}</div>
@@ -47,6 +47,12 @@ function displayForecast() {
 
 forecastHTML = forecastHTML + `</div>`;
 forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+let apiKey = "7784a4cd4aa2e0c25ead7bd96d585b8a";
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -68,6 +74,9 @@ function displayTemperature(response) {
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
     iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
+
+    getForecast(resonse.data.coord);
+
 }
 function search(city) {
     let apiKey = "7784a4cd4aa2e0c25ead7bd96d585b8a";
